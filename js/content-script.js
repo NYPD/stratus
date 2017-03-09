a13c1173c7ee4751605709b72727d10ba1a537b8
NYPD <nypd54@outlook.com>
2017-03-09 00:01:31 -0500

    Lot more functionality completed
    Need logo and a few tweaks left

--- js/content-script.js
+++ js/content-script.js
@@ -1,6 +1,6 @@
 const stratus = (function() {
 
-  let intialize = function () {
+  let intialize = function() {
     _insertControls();
     _initializeListeners();
   };
@@ -9,6 +9,7 @@ const stratus = (function() {
 
     let playControlsVolume = document.querySelector('.playControls__volume');
 
+    //Listener Handle
     let checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
     checkbox.className = 'stratus-input';
@@ -26,24 +27,96 @@ const stratus = (function() {
 
     playControlsVolume.insertAdjacentElement('afterend', divInputContainer);
 
+
+    //Slider Stuff
+    let stratusSlider = document.createElement('input');
+    stratusSlider.type = 'range';
+    stratusSlider.className = 'stratus-slider';
+
+    let stratusVolumeSpan = document.createElement('span');
+    stratusVolumeSpan.className = 'stratus-volume';
+
+    let stratusSliderContainer = document.createElement('div');
+    stratusSliderContainer.className = 'stratus-slider-container';
+
+    stratusSliderContainer.appendChild(stratusSlider);
+    stratusSliderContainer.appendChild(stratusVolumeSpan);
+
+    playControlsVolume.querySelector('.volume').insertAdjacentElement('beforeend', stratusSliderContainer);
+
   };
 
-  let _initializeListeners = function () {
+  let _initializeListeners = function() {
 
+    let stratusContainer = document.querySelector('.stratus-container');
     let stratusInput = document.querySelector('.stratus-input');
+    let stratusSlider = document.querySelector('.stratus-slider');
+
+    let playControlsVolume = document.querySelector('.playControls__volume');
+    let soundCloudVolumeDiv = playControlsVolume.querySelector('.volume');
+    let soundcloudVolumeButton = soundCloudVolumeDiv.querySelector('.volume__button');
+
+    stratusContainer.addEventListener('click', function(event) {
+
+      var target = event.target;
+      var isInput = target.nodeName === 'INPUT';
+      if(isInput) return false;
+
+      var clickEvent = new MouseEvent('click', {'bubbles': false});
+      stratusInput.dispatchEvent(clickEvent);
+
+      return false;
+    });
 
     stratusInput.addEventListener('change', function() {
 
-      var stratusScript = document.querySelector('.stratus-script');
-      if(stratusScript !== null) stratusScript.parentNode.removeChild(stratusScript);
+      playControlsVolume.classList.toggle('stratus');
 
-      var script = document.createElement('script');
-      script.type = 'text/javascript';
-      script.src = chrome.extension.getURL('js/test.js');
-      script.className = 'stratus-script';
-      (document.head || document.body || document.documentElement).appendChild(script);
+      var stratusNotEnabled = !playControlsVolume.classList.contains('stratus');
+      if (stratusNotEnabled) return false;
+
+      var volumeLevel = Number.parseInt(soundCloudVolumeDiv.getAttribute('data-level')) * 10;
+      stratusSlider.value = volumeLevel;
+
+      var changeEvent = new Event('change', {'bubbles': false });
+      stratusSlider.dispatchEvent(changeEvent);
 
     });
+
+    stratusSlider.addEventListener('change', _stratusSliderEventHandler);
+    stratusSlider.addEventListener('input', _stratusSliderEventHandler);
+
+    soundcloudVolumeButton.addEventListener('click', function(event) {
+
+      var stratusNotEnabled = !playControlsVolume.classList.contains('stratus');
+
+      if (stratusNotEnabled) return true;
+
+      var volumeIsNotMuted = !soundCloudVolumeDiv.classList.contains('muted');
+
+      if(volumeIsNotMuted) return false;
+
+      stratusSlider.value = 0;
+
+      var changeEvent = new Event('change', {'bubbles': true });
+      stratusSlider.dispatchEvent(changeEvent);
+
+      event.preventDefault();
+      event.stopPropagation();
+    });
+
+  };
+
+  let _stratusSliderEventHandler = function() {
+
+    var stratusScript = document.querySelector('.stratus-script');
+    if (stratusScript !== null) stratusScript.parentNode.removeChild(stratusScript);
+
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = chrome.extension.getURL('js/test.js');
+    script.className = 'stratus-script';
+    (document.head || document.body || document.documentElement).appendChild(script);
 
   };
 
--- js/test.js
+++ js/test.js
@@ -1,15 +1,21 @@
 var volumeSliderWrapper = document.querySelector('.volume__sliderWrapper');
 var currentVolume = Number.parseFloat(volumeSliderWrapper.getAttribute('aria-valuenow'), 10);
 
-var stratusInput = document.querySelector('.stratus-input');
-var volume = stratusInput.checked? (currentVolume / 2) : currentVolume;
+var stratusSlider = document.querySelector('.stratus-slider');
+
+var volume = stratusSlider.value;
 
 webpackJsonp([], {
   0: function(a, b, require) {
     var modules = require.c;
     for (var x in modules) {
-      if (modules[x].exports.broadcast)
-        modules[x].exports.broadcast('volume:set', volume);
+
+      if (! modules[x].exports.broadcast) continue;
+
+      modules[x].exports.broadcast('volume:set', (volume / 100));
+      break;
     }
   }
 });
+
+document.querySelector('.stratus-volume').innerText = volume;
