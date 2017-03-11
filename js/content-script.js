037aaabaf1ba8bd2f3af92319a4f1f2b1362b383
NYPD <nypd54@outlook.com>
2017-03-10 19:11:13 -0500

    Logo placement tweak on soundcloud page

--- js/content-script.js
+++ js/content-script.js
@@ -28,7 +28,7 @@ const stratus = (function() {
 
     let stratusIcon = document.createElement('img');
     stratusIcon.className = 'stratus-icon';
-    stratusIcon.src = chrome.extension.getURL('images/notext16.png');
+    stratusIcon.src = chrome.extension.getURL('images/notext24.png');
 
     let divInputContainer = document.createElement('div');
     divInputContainer.className = 'stratus-container';
